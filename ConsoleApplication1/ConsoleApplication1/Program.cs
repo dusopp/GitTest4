b6d3ae069a6bac2fd47a8f742eadf3b84a36bf27
dusopp <32883724+dusopp@users.noreply.github.com>
2018-06-11 21:25:49 +0200

    second edit in github

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             
-            //Code was added
+            //Code was edited in github
         }
     }
 }
